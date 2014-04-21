7e33ddd5496adb10b904d234c0cb5f7d0838d7df
Vincent <centzilius@w1r3.net>
2014-04-21 19:29:50 +0200

    forgot map.php

--- map.php
+++ map.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>Pegelcraft</title>
-	<link rel="stylesheet" href="css/yeti.bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<style type="text/css">
 		.table thead th {
 			background-color: #428BCA;
@@ -44,6 +44,8 @@
     </div>
 <iframe src="http://livemap.pegelf.de" width="100%" height="100%"  frameborder="10" vspace="0" hspace="0"
 marginwidth="0" marginheight="0" scrolling="no" noresize></iframe>
+<!-- Javascript -->
+<script href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<!-- End Javascript -->
 </body>
-
 </html>
