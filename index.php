6864a48f13133b48fb78a44b4a31a364c6e25e7b
Vincent <centzilius@w1r3.net>
2014-04-28 16:32:58 +0200

    I'm stupid and I know it #1

--- index.php
+++ index.php
@@ -104,7 +104,8 @@ include "templates/navbar.php";
       </div>
     </div>
 <!-- Javascript -->
-<script href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <!-- End Javascript -->
 </body>
 </html>
--- map.php
+++ map.php
@@ -40,7 +40,8 @@
 <iframe src="//livemap.pegelf.de" width="100%" height="100%"  frameborder="10" vspace="0" hspace="0"
 marginwidth="0" marginheight="0" scrolling="no" noresize></iframe>
 <!-- Javascript -->
-<script href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 <!-- End Javascript -->
 </body>
 </html>
