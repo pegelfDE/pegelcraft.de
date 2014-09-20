425375983dc9935b78cca4a4fd3c0a3b46ced0ee
Centzilius <Centzilius@users.noreply.github.com>
2014-09-20 14:49:44 +0200

    Update map.php

--- map.php
+++ map.php
@@ -1,41 +1,8 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-	<meta charset="utf-8">
-	<title>Pegelcraft</title>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-	<style type="text/css">
-		html,body,iframe {
-			min-height:100%;
-			height:100%;
-		}
-	</style>
-</head>
-
-<body>
-    <div class="navbar navbar-default navbar-static-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Pegelcraft</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="index.php">Home</a></li>
-            <li class="active"><a href="#">Map</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="https://pegelf.de">pegelf.de</a></li>
-            <li class="active"><a href="#">pegelcraft.de</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+<?php
+include "config.php";
+$page['navbarid'] = 2;
+include "templates/navbar.php";
+?>
 <iframe src="//livemap.pegelf.de" width="100%" height="100%"  frameborder="10" vspace="0" hspace="0"
 marginwidth="0" marginheight="0" scrolling="no" noresize></iframe>
 <!-- Javascript -->
